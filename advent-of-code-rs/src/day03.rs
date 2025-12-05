aa09743cc9bb142a45928ab811de60438bdcdc6a
Jan <44269935+jan-kn@users.noreply.github.com>
2025-12-05 15:56:43 +0100

    speed up day 03

--- advent-of-code-rs/src/day03.rs
+++ advent-of-code-rs/src/day03.rs
@@ -1,34 +1,36 @@
 use std::fs;
 
 /// Converts a slice of to base 10 number.
-fn slice_to_base_10(bank: &[u64]) -> u64 {
+fn slice_to_base_10(bank: &[u8]) -> u64 {
     bank.iter()
         .rev()
         .enumerate()
-        .map(|(e, &i)| i * 10u64.pow(e as u32))
+        .map(|(e, &i)| u64::from(i) * 10u64.pow(e as u32))
         .sum()
 }
 
-fn find_max_joltage(bank: &[u64], battery_count: usize) -> u64 {
-    let mut best = (&bank[bank.len() - battery_count..]).to_vec();
+// Removes digit `idx` in base 10.
+fn remove_digit(num: u64, idx: u32) -> u64 {
+    let len = num.ilog10() + 1;
+    let exp = 10u64.pow(len - 1 - idx);
+    let left = num / (exp * 10);
+    let right = num % exp;
+    left * exp + right
+}
+
+fn find_max_joltage(bank: &[u8], battery_count: usize) -> u64 {
+    let mut best = slice_to_base_10(&bank[bank.len() - battery_count..]);
     // iteratively prepend batteries from back to front
     for &battery in (&bank[0..(bank.len() - battery_count)]).iter().rev() {
         // find best battery to remove
-        let mut best_proposed = (0..battery_count)
-            .map(|idx_to_remove| {
-                best.iter()
-                    .enumerate()
-                    .filter_map(|(j, n)| if j != idx_to_remove { Some(*n) } else { None })
-                    .collect::<Vec<_>>()
-            })
-            .max_by_key(|bank| slice_to_base_10(bank))
+        let best_with_removed = (0u32..battery_count as u32)
+            .map(|idx_to_remove| remove_digit(best, idx_to_remove))
+            .max()
             .unwrap();
-        best_proposed.insert(0, battery);
-        if slice_to_base_10(&best_proposed) >= slice_to_base_10(&best) {
-            best = best_proposed
-        }
+        let proposed = (battery as u64) * 10u64.pow(battery_count as u32 - 1) + best_with_removed;
+        best = best.max(proposed);
     }
-    slice_to_base_10(&best)
+    return best;
 }
 
 #[cfg(test)]
@@ -36,9 +38,16 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_remove_digit() {
+        assert_eq!(remove_digit(15, 1), 1);
+        assert_eq!(remove_digit(15, 0), 5);
+        assert_eq!(remove_digit(1235, 1), 135);
+    }
+
+    #[test]
     fn test_find_max_n() {
         assert_eq!(find_max_joltage(&[9, 8, 7, 6, 5, 4], 2), 98);
-        assert_eq!(find_max_joltage(&[8, 0, 0, 0, 0, 9], 2), 89);
+        assert_eq!(find_max_joltage(&[8, 1, 1, 1, 1, 9], 2), 89);
         assert_eq!(find_max_joltage(&[2, 3, 4, 2, 7, 8], 2), 78);
         assert_eq!(find_max_joltage(&[1, 9, 1, 1, 2, 1], 2), 92);
         assert_eq!(find_max_joltage(&[9, 9, 1, 1, 2, 1], 2), 99);
@@ -71,19 +80,17 @@ mod tests {
 pub fn main() {
     let contents = fs::read_to_string("assets/input03.txt").unwrap();
 
-    let banks: Vec<Vec<u64>> = contents
-        .lines()
-        .map(|l| {
-            l.char_indices()
-                .map(|(i, c)| (&l[i..i + c.len_utf8()]).parse().unwrap())
-                .collect()
-        })
-        .collect();
+    // let banks = contents.lines().map(|l| l.as_bytes());
+    let banks = contents.lines().map(|l| {
+        l.char_indices()
+            .map(|(i, c)| (&l[i..i + c.len_utf8()]).parse().unwrap())
+            .collect::<Vec<_>>()
+    });
 
     // part 1
     // let n = 2;
     // part 2
     let n = 12;
-    let result: u64 = banks.iter().map(|b| find_max_joltage(b, n)).sum();
+    let result: u64 = banks.map(|b| find_max_joltage(&b, n)).sum();
     println!("{}", result);
 }
